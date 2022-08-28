038fce6b519296544beca5ca79545901c751e323
Clyde <dimich_vlg@mail.ru>
2022-08-28 15:34:14 +0300

    v t-57+

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+  before_action :authenticate_user!, except: [:show]
+  before_action :set_current_user, except: [:show]
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+  def set_current_user
+    @user = current_user
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  before_validation :set_name, on: :create
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  has_many :events
+  validates :name, presence: true, length: {maximum: 35}
+  
+  private
+
+  def set_name
+    self.name = "Товарисч №#{rand(777)}" if self.name.blank?
+  end
+end
