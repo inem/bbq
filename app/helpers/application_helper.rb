41a30f22c1eb10018edf19a7e444848a812a6bf7
Clyde <dimich_vlg@mail.ru>
2022-08-28 17:54:46 +0300

    v 57-1.1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
   def user_avatar(user)
     asset_path('user.png')
   end
+
+  def inclination(num, first_form, second_form, third_form)
+    last_digit = num % 10
+  
+    if last_digit == 1 && num != 11
+      first_form
+    elsif last_digit.between?(2, 4) && !(num % 100).between?(12, 14)
+      second_form
+    else
+      third_form
+    end
+  end
 end
