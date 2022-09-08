06423d2a34e7bc7048308d36ec0b5b8968675aad
Clyde <dimich_vlg@mail.ru>
2022-09-08 16:58:39 +0300

    email_is_busy added

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,8 +2,11 @@ class Subscription < ApplicationRecord
   belongs_to :event
   belongs_to :user, optional: true
   validates :user_name, presence: true, unless: -> { user.present? }
-  validates :user_email, presence: true, format: /\A[\w\-.]+@[\w\-.]+\z/, unless: -> { user.present? }
-  validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
+
+  with_options unless: -> { user.present? } do
+    validates :user_email, presence: true, format: /\A[\w\-.]+@[\w\-.]+\z/, uniqueness: {scope: :event_id}
+    validate :email_is_busy 
+  end
 
   with_options if: -> { user.present? } do
   validates :user, uniqueness: {scope: :event_id}
@@ -29,6 +32,12 @@ class Subscription < ApplicationRecord
   def is_owner_error_check
     if user == event.user
       errors.add(:user_id, :is_owner, message: I18n.t('controllers.subscriptions.self_sub'))
+    end
+  end
+
+  def email_is_busy
+    if User.where(email: user_email).present?
+      errors.add(:user_email, :is_busy, message: I18n.t('controllers.subscriptions.email_is_busy'))
     end
   end
 end
