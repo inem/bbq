3fc59679f070e49ea053a1c4041a985f610ada73
Clyde <dimich_vlg@mail.ru>
2022-09-14 01:24:56 +0300

    encrypted mailjet secrets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,7 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
+  config.action_mailer.delivery_method = :mailjet_api
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
