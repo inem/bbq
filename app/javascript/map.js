4515e69287e7b4dab97cb2a4711b94ee19f57195
Clyde <dimich_vlg@mail.ru>
2022-09-16 11:02:46 +0300

    yandex maps updated

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -1,10 +1,9 @@
 ymaps.ready(init);
-var myMap;
 
 function init(){
   address = document.getElementById('map').getAttribute('data-address');
 
-  myMap = new ymaps.Map("map", {
+  const myMap = new ymaps.Map("map", {
       center: [55.76, 37.64],
       zoom: 10
   });
